3d6ce066a3e1b5b5ae17c3fdb1d2b155bb48b09a
Samantha Arellano <Sambollano54@gmail.com>
2021-07-25 19:45:03 -0500

    .

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const routes = require('./controllers/')
-const sequelize = require('./config/connnection.js')
+const exphbs = require("express-handlebars");
 const path = require('path')
-const helpers = require('./utils/helpers');
-
+const helpers = require('./utils/helpers')
+const session = require('express-session')
 const app = express()
 
-const PORT = process.env.PORT || 3001
-
+const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connnection.js')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: 'secret',
@@ -20,13 +20,18 @@ const sess = {
 };
 
 app.use(session(sess));
-
+const hbs = exphbs.create({
+    helpers: {
+      format_date: date => {
+        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+      }
+    }
+  });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(routes);
+app.use(require('./controllers/'));
 
-// turn on connection to db and server
 sequelize.sync({
     force: false
 }).then(() => {

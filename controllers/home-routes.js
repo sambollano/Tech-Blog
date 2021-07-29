b30edac932d534f949c189c084ef5fc373f0bf66
Samantha Arellano <Sambollano54@gmail.com>
2021-07-29 13:04:23 -0500

    .

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
-const router = require('express').Router();
 const { Post, User, Comment, Vote } = require('../models');
+const router = require('express').Router();
 
 router.get('/', (req, res) => {
   console.log('======================');

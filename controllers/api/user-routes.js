42052955fa896e51e212c83de00f09d617885898
Samantha Arellano <Sambollano54@gmail.com>
2021-07-29 15:00:59 -0500

    Update user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,47 +1,28 @@
   
 const router = require('express').Router();
-const { User, Post, Comment, Vote } = require('../../models');
+const { User, Post } = require('../../models');
 
-// get all users
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   User.findAll({
-    attributes: { exclude: ['password'] }
+    attributes: { exclude: ['password'] },
+    include: [{
+      model: Post,
+    }]
   })
     .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   User.findOne({
-    attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
     },
-    include: [
-      {
-        model: Post,
-        attributes: ['id', 'title', 'post_url', 'created_at']
-      },
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'created_at'],
-        include: {
-          model: Post,
-          attributes: ['title']
-        }
-      },
-      {
-        model: Post,
-        attributes: ['title'],
-        through: Vote,
-        as: 'voted_posts'
-      }
-    ]
   })
-    .then(dbUserData => {
+  .then(dbUserData => {
       if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
         return;
@@ -53,9 +34,7 @@ router.get('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
-
-router.post('/', (req, res) => {
-  
+router.post("/", (req, res) => { 
   User.create({
     username: req.body.username,
     email: req.body.email,
@@ -76,11 +55,11 @@ router.post('/', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   User.findOne({
     where: {
       email: req.body.email
-    }
+    },
   }).then(dbUserData => {
     if (!dbUserData) {
       res.status(400).json({ message: 'No user with that email address!' });
@@ -113,46 +92,6 @@ router.post('/logout', (req, res) => {
   else {
     res.status(404).end();
   }
-});
-
-router.put('/:id', (req, res) => {
-
-  User.update(req.body, {
-    individualHooks: true,
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.delete('/:id', (req, res) => {
-  User.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
 });
 
 module.exports = router;

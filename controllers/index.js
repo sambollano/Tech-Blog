7fe3b7aa1f0ff875a08610facf68fbba872b4f3b
Samantha Arellano <Sambollano54@gmail.com>
2021-07-29 14:19:35 -0500

    .

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const apiRoutes = require('./api');
 const dashboardRoutes = require('./dashboard-routes.js');
-const userRoutes = require('express').Router();
+const userRoutes = require("./api/user-routes")
+const router = require('express').Router();
 
-router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api/users'), userRoutes();
 

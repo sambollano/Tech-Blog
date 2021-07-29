83073e607ac20fe32838b0b938a0e49d36703a66
Samantha Arellano <Sambollano54@gmail.com>
2021-07-28 21:29:36 -0500

    .

--- config/connnection.js
+++ config/connnection.js
@@ -9,12 +9,17 @@ if (process.env.JAWSDB_URL) {
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
+        port: 5501,
         user: 'root',
         dialect: 'mysql',
         dialectOptions: {
           decimalNumbers: true,
         },
       });
+      db.connect(function(err) {
+        if (err) throw err
+        console.log("You have connected to MySQL " + connection.threadId)
+    });
     }
     
-    module.exports = sequelize; 
+    module.exports = connnection; 

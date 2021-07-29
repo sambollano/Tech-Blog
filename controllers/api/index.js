580799c07cc0a4aa976e3f04775bcba39fb7f5ea
Samantha Arellano <Sambollano54@gmail.com>
2021-07-29 15:30:47 -0500

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-
-const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
+const userRoutes = require('./user-routes');
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+router.use('/posts', postRoutes);
+router.use('/users', userRoutes);
+
+
 
 module.exports = router;

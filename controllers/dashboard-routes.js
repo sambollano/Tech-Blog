c7e981706c5ca4e7c300a3855115949af607ef3e
Samantha Arellano <Sambollano54@gmail.com>
2021-07-29 14:49:37 -0500

    Update dashboard-routes.js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
-const { Post, User, Comment, Vote } = require('../models');
-const withAuth = require('../utils/auth');
+const { Post, User, Comment } = require('../models');
+const sequelize = require("../config/connnection");
 
-router.get('/', withAuth, (req, res) => {
-  console.log(req.session);
-  console.log('======================');
+router.get("/", (req, res) => {
   Post.findAll({
     where: {
       user_id: req.session.user_id
@@ -14,7 +12,6 @@ router.get('/', withAuth, (req, res) => {
       'post_url',
       'title',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
       {
@@ -22,18 +19,21 @@ router.get('/', withAuth, (req, res) => {
         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
-          attributes: ['username']
+          attributes: ["username"]
         }
       },
       {
         model: User,
-        attributes: ['username']
+        attributes: ["username"]
       }
     ]
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('dashboard', { posts, loggedIn: true });
+      res.render("dashboard", { 
+        posts, 
+        loggedIn: true 
+      });
     })
     .catch(err => {
       console.log(err);
@@ -41,14 +41,13 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
-router.get('/edit/:id', withAuth, (req, res) => {
+router.get('/edit/:id', (req, res) => {
   Post.findByPk(req.params.id, {
     attributes: [
       'id',
       'post_url',
       'title',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
       {
@@ -57,19 +56,19 @@ router.get('/edit/:id', withAuth, (req, res) => {
         include: {
           model: User,
           attributes: ['username']
-        }
+        },
       },
       {
         model: User,
         attributes: ['username']
-      }
-    ]
+      },
+    ],
   })
     .then(dbPostData => {
       if (dbPostData) {
         const post = dbPostData.get({ plain: true });
         
-        res.render('edit-post', {
+        res.render("edit-post", {
           post,
           loggedIn: true
         });
